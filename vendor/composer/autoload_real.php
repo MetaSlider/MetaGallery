5fbc8f42f71863c6a1128a3ac57b222432a878a3
KevinBatdorf@users.noreply.github.com <runner@fv-az176-222.tcy4nqwsb11u3bkt5we4bd1vfa.cx.internal.cloudapp.net>
2021-01-13 15:57:25 +0000

    Build Production Assets

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInitdee8288e668ea468e6b5d9e974f35d96
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitdee8288e668ea468e6b5d9e974f35d96', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitdee8288e668ea468e6b5d9e974f35d96', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitdee8288e668ea468e6b5d9e974f35d96::getInitializer($loader));
         } else {
