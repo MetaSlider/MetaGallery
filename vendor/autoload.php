d14c89de95328ae6a1425cd78e28787de0e86b42
KevinBatdorf@users.noreply.github.com <runner@fv-az58-311.zy5tiydptoxulpn2ih0dlbftoc.cx.internal.cloudapp.net>
2021-01-14 04:16:06 +0000

    Bundle up plugin

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit64a40f1fd0e59dc28d74d0252105fec5::getLoader();
+return ComposerAutoloaderInita474fe01e13245109c340615c127b948::getLoader();
